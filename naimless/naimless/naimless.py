7cf21bb3c1719ba6f306ff76bfba9d7242d1f089
Joan Clark-Nicolas <joan.clark88@gmail.com>
2024-02-23 11:55:48 +0000

    Now including atomic numbers

--- naimless/naimless/naimless.py
+++ naimless/naimless/naimless.py
@@ -1,6 +1,7 @@
 import sys
 import importlib
 import numpy as np
+from naimless.aidetools.periodic_table import Periodic_Table
 
 # defining path to import submodules
 
@@ -25,6 +26,7 @@ class NaiMLeSS:
         self.name = name
         self.n_atoms = n_atoms
         self.atom_names = atom_names
+        self.z = self.init_z(atom_names)
         self.R = R
         self.training_data = {}
         self.init_training_data(training_labels, qm_engine)
@@ -47,36 +49,8 @@ class NaiMLeSS:
                     print(error)
                     sys.exit()
 
-    """
-    def load_training_data(
-        self,
-        training_labels,
-    ) -> None:
-        # Add the current file path to sys.path
-        syspath0 = sys.path
-        project_dir = path.dirname(path.abspath(__file__))
-        if project_dir not in sys.path:
-            sys.path.append(project_dir)
-
-        # Load all the requested properties dict={module1:{function1:[],function2:[]},module2:{function3:[],function4:[]}}
-        for submodule_name, prop_names in training_labels.items():
-            try:
-                module = importlib.import_module(submodule_name)
-                self.traiing_data[module] = {}
-                for prop_name in prop_names:
-                    try:
-                        prop_func = getattr(module, prop_name)
-                        self.traiing_data[module][prop_func] = prop_func()
-                    except AttributeError:
-                        print(
-                            f"Warning: Property '{prop_name}' not found in '{submodule_name}'"
-                        )
-                        sys.exit()
-            except ModuleNotFoundError:
-                print(f"Warning: Submodule '{submodule_name}' does not exist")
-                sys.exit()
-
-        # reset sys.path
-        sys.path = syspath0
-        return
-    """
+    def init_z(self, atom_names: list) -> np.array:
+        z = []
+        for element in atom_names:
+            z.append(Periodic_Table.atomic_numbers[element])
+        return np.array(z)
